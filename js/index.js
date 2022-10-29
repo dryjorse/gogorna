9a577abafbf3307e4021d9115996efbcdb933ec7
dryjorse <kozoevabdulazim@gmail.com>
2022-10-30 01:49:49 +0300

    gogorna

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
 document.querySelector('.icon-menu').addEventListener('click', function () {
     document.querySelector('.menu__body').classList.toggle('open__menu')
 })
+
+let obj = {
+    name: 'Andrew',
+    age: 15,
+    inner: {
+        weight: 50,
+        size: 18,
+    }
+}
+
+let newed = {...obj, inner: {...obj.inner}}
+
+console.log(newed)
